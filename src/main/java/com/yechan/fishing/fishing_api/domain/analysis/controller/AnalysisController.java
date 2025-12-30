9bce0cc55c6c09185a31d09b1d3b828f10d5614c
yechan <rladPcks980@gmail.com>
2025-12-31 00:28:43 +0900

    fix: url에서 api제거

--- src/main/java/com/yechan/fishing/fishing_api/domain/analysis/controller/AnalysisController.java
+++ src/main/java/com/yechan/fishing/fishing_api/domain/analysis/controller/AnalysisController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/analysis")
+@RequestMapping("/v1/analysis")
 public class AnalysisController {
 
     private final AnalysisService analysisService;
--- src/main/java/com/yechan/fishing/fishing_api/domain/map/controller/MapController.java
+++ src/main/java/com/yechan/fishing/fishing_api/domain/map/controller/MapController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/map")
+@RequestMapping("/v1/map")
 public class MapController {
 
     private final MapService mapService;
